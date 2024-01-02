1d7a24919a85b285abafd1ac4e3d6d615d3ce59e
mahabub193 <102405335+mahabub193@users.noreply.github.com>
2024-01-03 00:26:09 +0600

    querySelectorAll and addEventListener

---
+++ index.js
@@ -0,0 +1,12 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("i got click");
+    });
+}
+
+
+
+// function handleClick() {
+//     alert("i got click");
+// }
