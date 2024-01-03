ab88058a09658d27cf78a1f7c03fdab5565b855f
mahabub193 <102405335+mahabub193@users.noreply.github.com>
2024-01-03 11:36:06 +0600

    keypress and button press

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
+//detecting button press
 var numberOfButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("i got click");
+        var buttonInnerHTML = this.innerHTML;
+        makeSounds(buttonInnerHTML);
+
     });
 }
 
 
+//detecting keyboard press
 
-// function handleClick() {
-//     alert("i got click");
-// }
+document.addEventListener("keydown", function (event) {
+    makeSounds(event.key);
+});
+
+function makeSounds(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+
+        case "d":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+
+        case "j":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+
+        case "k":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+
+        default: console.log(buttonInnerHTML);
+            break;
+    }
+}
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
